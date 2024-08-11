9c3be26eefc29e3141eac4feb89ae4811970fa00
Mats Eriksson <mats.eriksson.ingemar@gmail.com>
2024-08-11 16:35:29 +0200

    Create a language file

--- server/readEnvironment.mjs
+++ server/readEnvironment.mjs
@@ -12,7 +12,7 @@ export default function readEnvironment(path = ".env") {
             },
             SERVER_LANGUAGES: {
                 type: "string",
-                default: "en-US, sv-SE",
+                default: "en-US, sv-SE, fr-FR, de-DE",
                 description: "Which languages are the server supporting"
             },
             PRINT_ROUTES: {
