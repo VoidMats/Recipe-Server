4c0f4490c43da59953f97fe2769fb0de5f96ba81
Mats Eriksson <mats.eriksson.ingemar@gmail.com>
2024-09-09 21:57:47 +0200

    Add custom color to alert

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -16,14 +16,19 @@ export class Alert {
         this.type = type;
         this.message = message;
         this.alertElement = null;
+        const root = document.documentElement; 
+        const colorSuccess = getComputedStyle(root).getPropertyValue('--color-success').trim();
+        const colorError = getComputedStyle(root).getPropertyValue('--color-Error').trim();
+        const colorWarning = getComputedStyle(root).getPropertyValue('--color-Warning').trim();
+        const colorInfo = getComputedStyle(root).getPropertyValue('--color-Info').trim();
         this._options = {
             dismissible: (options.dismissable) ? options.dismissable : true,
             timeout: (options.timeout) ? options.timeout : 10000,
             color: {
-                success: (options.colorSuccess) ? options.colorSuccess : "#4caf50",
-                error: (options.colorError) ? options.colorError : "#f44336",
-                warning: (options.colorWarning) ? options.colorWarning : "#ff9800",
-                info: (options.colorInfo) ? options.colorInfo : "#2196f3"
+                success: (options.colorSuccess) ? options.colorSuccess : colorSuccess ?? "#4caf50",
+                error: (options.colorError) ? options.colorError : colorError ?? "#f44336",
+                warning: (options.colorWarning) ? options.colorWarning : colorWarning ?? "#ff9800",
+                info: (options.colorInfo) ? options.colorInfo : colorInfo ?? "#2196f3"
             }
         };
 
@@ -55,19 +60,19 @@ export class Alert {
           }
     
           .alert-success {
-            background-color: #4caf50;
+            background-color: ${this._options.color.success};
           }
     
           .alert-error {
-            background-color: #f44336;
+            background-color: ${this._options.color.error};
           }
     
           .alert-warning {
-            background-color: #ff9800;
+            background-color: ${this._options.color.warning};
           }
     
           .alert-info {
-            background-color: #2196f3;
+            background-color: ${this._options.color.info};
           }
     
           .alert-close {
@@ -77,6 +82,26 @@ export class Alert {
             color: white;
             font-size: 16px;
             cursor: pointer;
+          }
+
+          @keyframes slide-up {
+            from {
+              bottom: -100px; /* Start from below the screen */
+              opacity: 0; /* Start with 0 opacity */
+            }
+            to {
+              bottom: 20px; /* End at 20px above the bottom */
+              opacity: 1; /* End with full opacity */
+            }
+          }
+
+          @keyframes fade-out {
+            from {
+              opacity: 1;
+            }
+            to {
+              opacity: 0;
+            }
           }
         `;
         document.head.appendChild(style);
